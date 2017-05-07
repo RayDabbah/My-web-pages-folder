d933039d8f28099d9589a0f5646cc27256e038be
Rachamim Dabbah <raydabbah@gmail.com>
2017-05-07 13:14:34 -0400

    Completed my first AJAX call; it's functional

--- ajax.js
+++ ajax.js
@@ -1,8 +1,12 @@
-(function() {
-	var url = "http://api.openweathermap.org/data/2.5/weather?q=London,England";
+AJAX();
+setInterval(AJAX, 500000);
+
+function AJAX() {
+	var url = "http://api.openweathermap.org/data/2.5/weather?q=08701, US";
 	var apiKe = "e9b32cd985cc0132db4aea4282aac2f2";
 	makeRequest();
-    var httpRequest;
+    var httpRequest,
+	weatherBox = document.getElementById('weather');
 	function makeRequest() {
 		httpRequest = new XMLHttpRequest();
 		httpRequest.onreadystatechange = responseMethod;
@@ -11,7 +15,25 @@
 	}
 	function responseMethod() {
 		if (httpRequest.readyState === 4) {
-			console.log(httpRequest.responseText);
+			
+		if (httpRequest.status === 200) {
+			updateWeather(httpRequest.responseText);
+		} else {
+			hideElement();
 		}
+			console.log(httpRequest.responseText);
 	}
-})();
+	}
+	function updateWeather(responseText) {
+	var response = JSON.parse(responseText);
+	var	conditions = response.weather[0].description,
+		temperatureC = response.main.temp - 273.15,
+		tempF = Math.floor(temperatureC * 1.8 + 32),
+		windSpeed = response.wind.speed;
+		weatherBox.innerHTML = '<p> Temperature ' + tempF + '&#176; F</p><br><p>' + conditions + '</p><br><p>Wind Speed: ' + windSpeed + ' MPH</p>';
+	}
+	function hideElement() {
+		weatherBox.innerHTML = '';
+	}
+	
+}
