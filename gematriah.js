7af3bf7047fd23298d21fc7e872b352ea8b2e7ff
raydabbah <dabbahray@yahoo.com>
2017-09-17 16:23:35 -0400

    מספר קטן works

--- gematriah.js
+++ gematriah.js
@@ -5,10 +5,27 @@ let result2 = document.getElementById('result2');
 const secondInput = document.getElementById('second-input');
 const compareButton = document.getElementById('compare');
 const resultDifference = document.getElementById("result-difference");
+const nav = document.querySelector('nav');
+const regular = document.getElementById('regular');
+const atBash = document.getElementById('at-bash');
+const misK = document.getElementById('mis-k');
 let toggleDisplay = false;
+let misparK = {};
 const letters = {
     א: 1, ב: 2, ג: 3, ד: 4, ה: 5, ו: 6, ז: 7, ח: 8, ט: 9, י: 10, כ: 20, ל: 30, מ: 40, נ: 50, ס: 60, ע: 70, פ: 80, צ: 90, ק: 100, ר: 200, ש: 300, ת: 400,
 }
+
+//  מספר קטן
+
+
+let misparNum = Object.values(letters).map(letter => {
+    return parseInt(String(letter).split('').filter(notZero => notZero != '0'))
+
+})
+misparNum.forEach((number, i) => {
+    return misparK[Object.keys(letters)[i]] = misparNum[i];
+});
+
 let calculateLetters = letters;
 const endLetters = {
     ן: calculateLetters.נ,
@@ -17,6 +34,19 @@ const endLetters = {
     ף: calculateLetters.פ,
     ך: calculateLetters.כ,
 }
+
+nav.addEventListener('click', () => {
+    if(misK.checked == true){
+        calculateLetters = misparK;
+    }if(regular.checked == true){
+        calculateLetters = letters;
+    }
+    calculate(input1, result1, 'input');
+    result1.innerHTML = calculateSum(input1);
+    result2.innerHTML = calculateSum(input2);
+})
+// calculate(nav, result1, 'change');
+// calculate(nav, result2, 'change');
 calculate(input1, result1, 'input');
 calculate(input2, result2, 'input');
 input1.addEventListener('input', difference);
@@ -52,29 +82,35 @@ function calculateSum(input) {
     }, 0)
 }
 
-function difference(){
-    if(toggleDisplay== true) resultDifference.textContent = Math.abs(result1.textContent - result2.textContent);
-    if(toggleDisplay == false) resultDifference.textContent = '';
-    if((result1.textContent == result2.textContent) && result1.textContent != "0"){
+function difference() {
+    if (toggleDisplay == true) resultDifference.textContent = Math.abs(result1.textContent - result2.textContent);
+    if (toggleDisplay == false) resultDifference.textContent = '';
+    if ((result1.textContent == result2.textContent) && result1.textContent != "0") {
         resultDifference.innerHTML = '&#10004';
         resultDifference.style.color = 'green';
         resultDifference.style.fontSize = '4em';
         result1.style.color = 'green';
         result2.style.color = 'green';
     }
-    if(resultDifference.textContent == "0"){
+    if (resultDifference.textContent == "0") {
         resultDifference.textContent = '';
     }
-    if(parseInt(result1.textContent)> parseInt(result2.textContent)){
+    if (parseInt(result1.textContent) > parseInt(result2.textContent)) {
         resultDifference.style.color = 'red';
         resultDifference.style.fontSize = '1.7em';
         result1.style.color = 'green';
         result2.style.color = 'red';
     }
-    if(parseInt(result2.textContent)> parseInt(result1.textContent)){
+    if (parseInt(result2.textContent) > parseInt(result1.textContent)) {
         resultDifference.style.color = 'red';
         resultDifference.style.fontSize = '1.7em';
         result1.style.color = 'red';
         result2.style.color = 'green';
     }
 }
+
+// function changeMethod(event, element){
+//     element.addEventListener(event, ()=>{
+//         if
+//     })
+// }
